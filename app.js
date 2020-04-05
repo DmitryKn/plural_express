b3157965dea8351cfb557f35040ee9836a56702a
DmitryKn <4dmitry.kn@gmail.com>
2020-04-05 13:18:01 -0400

    add static styles

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
-
+const path = require('path');
+const dotenv = require('dotenv');
 const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+app.use(express.static(path.join('public')));
+app.use(
+    '/css',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
+app.use(
+    '/js',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+);
+app.use(
+    '/js',
+    express.static(path.join(__dirname, 'node_modules/jquery/dist/'))
+);
 
 app.get('/', (req, res) => {
-    res.send('Hello there');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, () => {
-    console.log('Server running. Port 3000');
+    console.log(`Server running. Port: ${port}, data: ${process.env.TESTDATA}`);
 });
